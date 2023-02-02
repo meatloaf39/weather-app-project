44d0e29d8f40a26dd9be914c30b7df5912e15221
meatloaf39 <118136258+meatloaf39@users.noreply.github.com>
2023-02-02 21:30:47 +0100

    Misc update

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ function displayForecast(response) {
         src= "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
           forecastDay.condition.icon
         }.png"
-        alt="" 
+        alt="${forecastDay.condition.description}" 
         width="40">
       <div class="weather-forecast-temperature"> 
       <span class="weather-forecast-temperature-max">${Math.round(
